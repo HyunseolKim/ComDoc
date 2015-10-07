43a62f93c0608c63e02098ce97e49c71db1ba42d
가혜 <dnlrkgp@gmail.com>
2015-10-07 19:12:37 +0900

    UI: Login: Make fundamental ui about login, main activity
    로그인에서 확인 버튼을 누르면 메인뷰로 넘어간다. 메인뷰에서 네비게이션 버튼을 누르면 네비게이션 뷰로 들어가진다. 네비게이션 뷰의 드로어 부분에 메뉴들이 있고, 현재 견적정보를 클릭하면 견적정보 액티비티로 넘어간다.

---
+++ comdoc-android/app/src/main/java/com/example/kahye/comdoc/model/Example.java
@@ -0,0 +1,19 @@
+package com.example.kahye.comdoc.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kahye on 2015. 10. 7..
+ */
+public class Example {
+    String name;
+    String age;
+    int photoId;
+
+   public  Example(String name, String age, int photoId) {
+        this.name = name;
+        this.age = age;
+        this.photoId = photoId;
+    }
+}
