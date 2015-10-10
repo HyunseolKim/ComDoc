e5c4c1139993302c59187a39071d011191648a92
Min JJong <alswhdgus10@gmail.com>
2015-10-10 17:44:03 +0900

    s

--- comdoc-api-hub/api/controllers/UserController.js
+++ comdoc-api-hub/api/controllers/UserController.js
@@ -44,8 +44,8 @@ module.exports = {
           req.session.me = user.id;
 
           // All done- let the client know that everything worked.
-          // return res.ok();
-          return res.json(500, { error: 'message' });
+          return res.ok();
+          // return res.json(500, { error: 'message' });
         }
       });
     });
@@ -142,5 +142,49 @@ module.exports = {
       return res.backToHomePage();
 
     });
+  },
+
+  /**
+   * Check the provided email address and password, and if they
+   * match a real user in the database, sign in to Activity Overlord.
+   */
+  loginAPI: function (req, res) {
+
+    // Try to look up user using the provided email address
+    User.findOne({
+      email: req.param('email')
+    }, function foundUser(err, user) {
+      if (err) return res.negotiate(err);
+      if (!user) return res.notFound();
+
+      // Compare password attempt from the form params to the encrypted password
+      // from the database (`user.password`)
+      require('machinepack-passwords').checkPassword({
+        passwordAttempt: req.param('password'),
+        encryptedPassword: user.encryptedPassword
+      }).exec({
+
+        error: function (err){
+          return res.negotiate(err);
+        },
+
+        // If the password from the form params doesn't checkout w/ the encrypted
+        // password from the database...
+        incorrect: function (){
+          return res.notFound();
+        },
+
+        success: function (){
+
+          // Store user id in the user session
+          req.session.me = user.id;
+
+          // All done- let the client know that everything worked.
+          // return res.ok();
+          return res.json(500, { error: 'message' });
+        }
+      });
+    });
+
   }
 };
