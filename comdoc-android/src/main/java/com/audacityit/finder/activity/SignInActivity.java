d31e8d581e340988f2970955dc57e039def5fc1b
가혜 <dnlrkgp@gmail.com>
2015-10-13 17:17:56 +0900

    request: signup: sucess singup request
    회원가입 요청과 로그인 요청 완료. 그 외 텍스트 변경

--- comdoc-android/src/main/java/com/audacityit/finder/activity/SignInActivity.java
+++ comdoc-android/src/main/java/com/audacityit/finder/activity/SignInActivity.java
@@ -235,13 +235,14 @@ public class SignInActivity extends Activity implements View.OnClickListener, Vi
 
         RequestParams param = new RequestParams();
 
-        param.put("email", "test@gmail.com");
-        param.put("password", "123123");
+        param.put("email", mobileNumber);
+        param.put("password", password);
+
         StringEntity entity=new StringEntity(param.toString());
         mHttpClient.post("http://40.74.139.156:1337/login", param, new AsyncHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                        Toast.makeText(getApplicationContext(), "토스트메시지입니다.",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), statusCode,Toast.LENGTH_SHORT).show();
                         signInCompleteListener.onSignInComplete();
                         startActivity(new Intent(SignInActivity.this, HomeActivity.class));
                         finish();
@@ -249,7 +250,6 @@ public class SignInActivity extends Activity implements View.OnClickListener, Vi
 
                     @Override
                     public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                        //Log.e("POST요청 에러",  "" + e.getMessage());
                         Toast.makeText(getApplicationContext(), "fial.",Toast.LENGTH_SHORT).show();
                     }
                 });
